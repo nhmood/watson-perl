f62406b49b21cfcd3c0c4c95fe800e2a6067e611
nhmood <nhmood89@gmail.com>
2013-01-31 00:58:29 -0500

    Finished parsing block functionality and updated sample folder
    Finished parseDirs and parseFile blocks and set up sample parsing.
    Returns data in desired data format and ready for display.
    Also updated sample folder for better testing of empty/invalid
    source files

---
+++ src/a.cpp
@@ -0,0 +1,16 @@
+#[reviewme] - something here
+# just a comment
+# more comments
+actual code
+code
+code
+##[reviewme] - this is the second comment
+#[reviewme] - one more comment
+fjkdls
+jkflds
+fjdksl
+fdjskl
+#[reviewme] - this is the last commend
+
+#[fixme] - some stuff
+#[fixme] - some stuff 2
